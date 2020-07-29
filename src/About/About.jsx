de5686af859ff348d304528ff6551be2cd4408da
kisbel <kisbel46@gmail.com>
2020-07-29 13:35:57 -0400

    added some info on about me page, completed contact page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import './About.css';
+import kispic from '../Pictures/kispic.jpg';
 
 function About () {
     return (
         <>
         <Nav />
             <div id="ph1">about kisbel</div>
+
+            <div id="kis-info">
+                <img src={kispic} alt="picture of kisbel"></img>
+                <br />
+                <p1>Hi! I'm Kisbel. I recently graduated from Wyncode Academy's Full Stack Web Development program.
+                I have experience using React, Javascript, HTML, CSS (to name a few) - if you'll click on the LinkedIn, you'll see the variety
+                of skills I've used in my work. 
+
+                Before I started the full stack development program, I was a research coordinator at a university for a research project. 
+                While I do enjoy coding, I would ideally like to combine my two paths - managing and coding - and become a tech PM.
+                </p1>
+            </div>
         <Footer />
         </>
     )
--- src/Contact/ContactPage.jsx
+++ src/Contact/ContactPage.jsx
@@ -7,7 +7,34 @@ function ContactPage () {
     return (
         <>
             <Nav />
+
             <div id="ph1">contact</div>
+
+            <div id="form-area">
+                <form action="https://formspree.io/xwkraggk" method="POST">
+                    <div class="form-input">
+                        <label for="your_name">name:</label>
+                        <input id="your_name" type="text" name="your_name" autocomplete="off"></input>
+                    </div>
+
+                    <div class="form-input">
+                        <label for="your_email">email address:</label>
+                        <input id="your_email" type="email" name="your_email" autocomplete="off"></input>
+                    </div>
+
+                    <div class="form-input">
+                        <label for="your_message">message:</label>
+                        <textarea name="your_message" id="your_message" cols="40" rows="10"></textarea>
+                    </div>
+
+                    <div class="buttons">
+                        <button type="submit">submit</button>
+
+                        <button type="reset">reset</button>
+                    </div>
+                </form>
+            </div>
+
             <Footer />
         </>
     )
